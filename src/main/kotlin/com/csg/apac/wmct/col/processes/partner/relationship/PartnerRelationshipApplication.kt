eeceeb1548a260b6134bae78a9294e2c4cf2d203
Raghuveer Singh <raghu1213@hotmail.com>
2023-07-21 10:51:12 +0800

    Initial commit

---
+++ src/main/kotlin/com/csg/apac/wmct/col/processes/partner/relationship/PartnerRelationshipApplication.kt
@@ -0,0 +1,11 @@
+package com.csg.apac.wmct.col.processes.partner.relationship
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PartnerRelationshipApplication
+
+fun main(args: Array<String>) {
+  runApplication<PartnerRelationshipApplication>(*args)
+}
---
+++ src/test/kotlin/com/csg/apac/wmct/col/processes/partner/relationship/PartnerRelationshipApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.csg.apac.wmct.col.processes.partner.relationship
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PartnerRelationshipApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
